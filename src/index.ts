c7953b26fd8cc5c97651bd2faea6a4ed1c769d93
arturbien <artur.bien@gmail.com>
2023-03-11 13:37:18 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,73 @@
-type State = {
-  browserFontSize: number;
-  ratio: number;
-};
-type OnChangeHandler = (state: State) => void;
-
 class BrowserFontSizeObserver {
-  static #customElementName = "browser-font-size";
-  static #pixel: HTMLElement | null = null;
+  #customElement: HTMLElement;
+  resizeObserver: ResizeObserver | null = null;
 
-  static #createPixel() {
-    if (!BrowserFontSizeObserver.#pixel) {
-      const customElement = document.createElement(
-        BrowserFontSizeObserver.#customElementName
-      );
-      Object.assign(customElement.style, {
-        position: "fixed",
-        left: "0",
-        top: "0",
-        width: "0px",
-        height: "0px",
-        overflow: "hidden",
-        pointerEvents: "none",
-        clip: "rect(0 0 0 0)",
-        clipPath: "inset(50%)",
-      });
-
-      const shadowRoot = customElement.attachShadow({ mode: "closed" });
-      const iframe = document.createElement("iframe");
-      shadowRoot.appendChild(iframe);
-
-      document.documentElement.appendChild(customElement);
-      iframe.contentWindow.document.write(
-        `<div style="width:1rem;height:1rem;"></div>`
-      );
-      const pixel = iframe.contentWindow.document.querySelector(
-        "div"
-      ) as HTMLDivElement;
-
-      BrowserFontSizeObserver.#pixel = pixel;
-    }
-    return BrowserFontSizeObserver.#pixel;
+  constructor(
+    private readonly onChangeHandler: (state: {
+      browserFontSize: number;
+      ratio: number;
+    }) => void
+  ) {
+    this.#init();
   }
 
-  #state: State | null = null;
-  #resizeObserver: ResizeObserver | null = null;
-  #onChangeHandler: OnChangeHandler | null = null;
-
-  #setState(size: number) {
-    this.#state = {
-      browserFontSize: size,
-      ratio: size / 16,
-    };
-  }
-
-  constructor(onChangeHandler?: OnChangeHandler) {
-    const pixel = BrowserFontSizeObserver.#createPixel();
-    const size = pixel.offsetWidth;
-    this.#setState(size);
-
-    if (onChangeHandler) {
-      this.#onChangeHandler = onChangeHandler;
-    }
-    this.#resizeObserver = new ResizeObserver((entries) => {
+  async #init() {
+    const pixel = await this.#createPixel();
+    this.resizeObserver = new ResizeObserver((entries) => {
       for (const entry of entries) {
         const size = entry.borderBoxSize
           ? entry.borderBoxSize[0].inlineSize
           : (entry.target as HTMLElement).offsetWidth;
-
-        this.#setState(size);
-        this.#onChangeHandler?.(this.#state);
+        this.onChangeHandler({ browserFontSize: size, ratio: size / 16 });
       }
     });
-    this.#resizeObserver.observe(pixel);
+    this.resizeObserver.observe(pixel);
+  }
+
+  async #createPixel() {
+    this.#customElement = document.createElement("browser-font-size");
+    Object.assign(this.#customElement.style, {
+      position: "fixed",
+      left: "0",
+      top: "0",
+      width: "0px",
+      height: "0px",
+      overflow: "hidden",
+      pointerEvents: "none",
+      clip: "rect(0 0 0 0)",
+      clipPath: "inset(50%)",
+    });
+    const shadowRoot = this.#customElement.attachShadow({ mode: "closed" });
+    const iframe = document.createElement("iframe");
+    iframe.src = "about:blank";
+    shadowRoot.appendChild(iframe);
+    document.documentElement.appendChild(this.#customElement);
+
+    const createPixelElement = (document: Document) => {
+      document.open();
+      document.write(`<div style="width:1rem;height:1rem;"></div>`);
+      document.close();
+      const pixel = document.querySelector("div") as HTMLDivElement;
+      return pixel;
+    };
+
+    if (iframe.contentDocument) {
+      return createPixelElement(iframe.contentDocument);
+    } else {
+      return new Promise<HTMLDivElement>((resolve) => {
+        iframe.addEventListener("load", () => {
+          iframe.onload = null;
+          const pixel = createPixelElement(iframe.contentDocument);
+          resolve(pixel);
+        });
+      });
+    }
   }
 
   disconnect() {
-    this.#onChangeHandler = null;
-  }
-
-  get state() {
-    return this.#state;
+    this.resizeObserver?.disconnect();
+    this.resizeObserver = null;
+    this.#customElement.remove();
   }
 }
 
--- src/main.test.ts
+++ src/main.test.ts
@@ -1,9 +1,21 @@
 import { JSDOM } from "jsdom";
 import BrowserFontSizeObserver from "./";
 
-test("A pixel is created", () => {
-  const browserFontSizeObserver = new BrowserFontSizeObserver();
+afterEach(() => {
+  document.getElementsByTagName("html")[0].innerHTML = "";
+});
+
+test("A custom element is added to the DOM when an instance of BrowserFontSizeObserver is created", () => {
+  const browserFontSizeObserver = new BrowserFontSizeObserver(() => {});
   const customElement =
     document.documentElement.querySelector("browser-font-size");
   expect(customElement).toBeTruthy();
+});
+
+test("Custom element is removed from the DOM when observer is disconnected", () => {
+  const browserFontSizeObserver = new BrowserFontSizeObserver(() => {});
+  browserFontSizeObserver.disconnect();
+  const customElement =
+    document.documentElement.querySelector("browser-font-size");
+  expect(customElement).toBeFalsy();
 });
